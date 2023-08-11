aab28b8b4c781db48d8cff70831a92c7b4ade3f1
Jillian Anderson <12687428+jillianderson8@users.noreply.github.com>
2023-08-11 14:59:01 -0700

    Hacky code for generating model evaluation numbers

---
+++ object_detection_scripts/4_comparing_manual_counts/calc_mAP/src/calc_metrics.py
@@ -0,0 +1,329 @@
+from pathlib import Path
+import pandas as pd
+import ast
+import re
+import pickle as pkl
+import numpy as np
+from pycocotools.cocoeval import COCOeval
+from pycocotools.coco import COCO
+import matplotlib.pyplot as plt
+from PIL import Image
+import json
+
+# Bounding Box Helpers
+class BoundingBox:
+    def __init__(self):
+        self.x0 = 0
+        self.y0 = 0
+        self.x1 = 0
+        self.y1 = 0
+
+        self.x_center = 0
+        self.y_center = 0
+
+        self.width = 0
+        self.height = 0
+
+    def load_four_corners(self, x0, y0, x1, y1):
+        self.x0 = x0
+        self.y0 = y0
+        self.x1 = x1
+        self.y1 = y1
+
+        self.width = x1 - x0
+        self.height = y1 - y0
+
+        self.x_center = (x1 + x0) / 2
+        self.y_center = (y1 + y0) / 2
+
+    def four_corners(self):
+        return self.x0, self.y0, self.x1, self.y1
+
+    def geom(self):
+        pass
+
+
+def resize_annotation(row, tile_directory, rescale_factor):
+    raw_anno = ast.literal_eval(row['detection_boxes'])
+    raw_path = row['img.img_path']
+    img_date = re.search("[0-9]{8}", raw_path).group()
+    tile_name = Path(raw_path).name
+
+    tile = Image.open(tile_directory.joinpath(img_date).joinpath(tile_name))
+    tile_y, tile_x, _ = tile_name.split('.')
+    tile_w, tile_h = tile.size
+
+    tile_x0, tile_y0, tile_x1, tile_y1 = raw_anno
+    x0 = tile_x0*tile_w / rescale_factor
+    y0 = tile_y0*tile_h / rescale_factor
+    x1 = tile_x1*tile_w / rescale_factor
+    y1 = tile_y1*tile_h / rescale_factor
+
+    return x0, y0, x1, y1
+
+
+def resize_detection(row, tile_size=(1000, 1000), rescale_factor=1):
+    raw_detection = ast.literal_eval(row['detection_boxes'])
+    raw_path = row['image']
+    tile_name = Path(raw_path).name
+    tile_y, tile_x, _ = tile_name.split('.')
+    tile_w, tile_h = tile_size
+    tile_x0, tile_y0, tile_x1, tile_y1 = raw_detection
+    tile_x0 = tile_x0-int(tile_x)
+    tile_y0 = tile_y0-int(tile_y)
+    tile_x1 = tile_x1-int(tile_x)
+    tile_y1 = tile_y1-int(tile_y)
+
+    x0 = (tile_x0*tile_w + (int(tile_x)*tile_w)) / rescale_factor
+    y0 = (tile_y0*tile_h + (int(tile_y)*tile_h)) / rescale_factor
+    x1 = (tile_x1*tile_w + (int(tile_x)*tile_w)) / rescale_factor
+    y1 = (tile_y1*tile_h + (int(tile_y)*tile_h)) / rescale_factor
+
+    return x0, y0, x1, y1
+
+
+def convert_annotations(dataframes):
+    all_annotations = pd.concat(dataframes).dropna(subset=['detection_boxes']).apply(annotation_to_json,
+                                                                               axis=1)
+
+    return all_annotations
+
+
+def annotation_to_json(row):
+    # Get X, Y, X Y box & Resize it
+    resized_anno = resize_annotation(row, tile_directory, rescale_factor=1)
+    # Convert it to COCO box
+    bbox = x1_y1_x2_y2_conversion(resized_anno)
+
+    img_date = re.search("[0-9]{8}", row['img.img_path']).group()
+    annotation = {
+        'id': row['anno.idx'],
+        'image_id': int(img_date),
+        'category_id': label_map[ast.literal_eval(row['anno.lbl.name'])[0]],
+        'area': bbox[2] * bbox[3],
+        'bbox': bbox,
+        'iscrowd': 0
+    }
+
+    return annotation
+
+
+def detection_to_json(row):
+    # Get X, Y, X Y box & Resize it
+    resized_detection = resize_detection(row, tile_size=(1000,1000), rescale_factor=1)
+
+    # Convert it to COCO box
+    bbox = x1_y1_x2_y2_conversion(resized_detection)
+
+    img_date = re.search("[0-9]{8}", row['image']).group()
+    result = {
+        "image_id": int(img_date),
+        "category_id": row['detection_classes'],
+        "bbox": bbox,
+        "score": row['detection_scores']
+    }
+
+    return result
+
+
+def convert_detections(dataframes):
+    all_detections = pd.concat(dataframes)
+    # all_detections = all_detections[all_detections['detection_scores'] >= 0.2]
+    all_detections = all_detections.apply(detection_to_json, axis=1)
+    return all_detections
+
+
+def x1_y1_x2_y2_conversion(bbox):
+    x1, y1, x2, y2 = bbox
+    # x = (x2 + x1) / 2
+    # y = (y1 + y2) / 2
+    width = x2 - x1
+    height = y2 - y1
+
+    return [x1, y1, width, height]
+
+
+# Other Helpers
+def read_label_map(label_map_path):
+    item_id = None
+    item_name = None
+    items = {}
+
+    with open(label_map_path, "r") as file:
+        for line in file:
+            line.replace(" ", "")
+            if line == "item{":
+                pass
+            elif line == "}":
+                pass
+            elif "id" in line:
+                item_id = int(line.split(":", 1)[1].strip())
+            elif "name" in line:
+                item_name = line.split(":", 1)[1].replace("'", "").strip()
+
+            if item_id is not None and item_name is not None:
+                items[item_name] = item_id
+                item_id = None
+                item_name = None
+
+    return items
+
+
+def COCO_dict():
+    coco = {
+        "info": {},
+        "images": [],
+        "annotations": [],
+        "license": []
+    }
+    return coco
+
+
+# Metrics
+
+
+# Label Map
+label_map_path = '/Users/jilliana/Documents/rcg_projects/RuthJoy/Cormorants/cormorants-nesting-scripts-bkp/object_detection_scripts/2_training_pipeline/lost_to_tf/input/snb5/label_map.pbtxt'
+label_map = read_label_map(label_map_path)
+label_map['Cormorant DCCO'] = 0
+label_map['Cormorant PECO'] = 0
+label_map['Cormorant DCCO Juvenile'] = 0
+label_map['Cormorant DCCO Adult'] = 0
+label_map['Cormorant PECO Adult'] = 0
+
+label_map['Nest PECO Occupied'] = 1
+label_map['Nest DCCO Occupied'] = 1
+label_map['Nest DCCO'] = 1
+
+
+def create_annotation_coco_json(tile_dir, annotation_dir, out_file):
+    all_annotations = [pd.read_csv(f) for f in Path(annotation_dir).glob("*_annos.csv")]
+    coco_data = COCO_dict()
+
+    coco_data['annotations'] = list(convert_annotations(all_annotations))
+    coco_data['images'] = [{"id": id} for id in
+                           set([x['image_id'] for x in coco_data['annotations']])]
+    coco_data['categories'] = [{"id": v, "name": k} for k, v in label_map.items()]
+
+    with open(Path(out_file).expanduser(), "w") as f:
+        json.dump(coco_data, f)
+
+
+def create_detection_coco_json(tile_dir, detections_dir, out_file):
+    all_detections = [pd.read_csv(f) for f in Path(detections_dir).glob("merged*")]
+    results = list(convert_detections(all_detections))
+    with open(Path(out_file).expanduser(), "w") as f:
+        json.dump(results, f)
+
+
+def evaluate_from_json_file(annotation_json_file, detection_json_file, out_file=None,
+                            max_detections=None, iou_thresholds=None):
+    # Ground Truth COCO Object
+    cocoGT = COCO(Path(annotation_json_file).expanduser())
+
+    # Detection COCO Object
+    with open(Path(detection_json_file).expanduser(), 'r') as d_file:
+        detection_results = json.load(d_file)
+    cocoDt = cocoGT.loadRes(detection_results)
+
+    # COCO Eval Object
+    coco_eval = COCOeval(cocoGt=cocoGT, cocoDt=cocoDt, iouType="bbox")
+    if max_detections:
+        coco_eval.params.maxDets = max_detections
+    if iou_thresholds:
+        coco_eval.params.iouThrs = iou_thresholds
+
+    # Evaluation
+    coco_eval.evaluate()
+    coco_eval.accumulate()
+    coco_eval.summarize()
+
+    # Write out
+    if out_file:
+        with open(out_file, "wb") as f:
+            pkl.dump(coco_eval, f)
+
+    return coco_eval
+
+
+def calculate_precisions(coco_eval, iou_threshold=None):
+    if iou_threshold:
+        iou_index = np.where(np.array(coco_eval.params.iouThrs) == iou_threshold)[0]
+        total_map = coco_eval.eval['precision'][iou_index, :, :, 0, 2].mean()
+        print(f"mAP{iou_threshold}: \t\t {total_map:.1%}")
+
+    else:
+        total_map = coco_eval.eval['precision'][:, :, :, 0, 2].mean()
+        print(f"mAP: \t\t\t {total_map:.1%}")
+
+    # Classes
+    for i in coco_eval.params.catIds:
+        if iou_threshold:
+            map_cat = coco_eval.eval['precision'][iou_index, :, i, 0, 2].mean()
+            print(f"mAP@{iou_threshold}[{i}]: \t {map_cat:.1%}")
+        else:
+            map_cat = coco_eval.eval['precision'][:, :, i, 0, 2].mean()
+            print(f"mAP[{i}]: \t\t {map_cat:.1%}")
+
+
+    coco_eval.eval['precision'][0, :, :, 0, 2].mean()  # mAP @ 0.5
+    coco_eval.eval['precision'][0, :, 0, 0, 2].mean()  # mAP @ 0.5 Cormorant
+    coco_eval.eval['precision'][0, :, 1, 0, 2].mean()  # mAP @ 0.5 Nest
+
+
+def calculate_recalls(coco_eval, max_detections, iou_threshold=None):
+    det_index = np.where(np.array(coco_eval.params.maxDets)==max_detections)[0]
+
+    if iou_threshold:
+        iou_index = np.where(np.array(coco_eval.params.iouThrs) == iou_threshold)[0]
+        total_ar = coco_eval.eval['recall'][iou_index, :, 0, det_index].mean()
+        print(f"AR@{max_detections} @{iou_threshold}IoU: \t {total_ar:.1%}")
+
+    else:
+        total_ar = coco_eval.eval['recall'][:, :, 0, det_index].mean()
+        print(f"AR@{max_detections}: \t {total_ar:.1%}")
+
+    # Classes
+    for i in coco_eval.params.catIds:
+        if iou_threshold:
+            ar_cat = coco_eval.eval['recall'][iou_index, i, 0, det_index].mean()
+            print(f"AR@{max_detections}[{i}] @{iou_threshold}IoU: \t {ar_cat:.1%}")
+        else:
+            ar_cat = coco_eval.eval['recall'][:, i, 0, det_index].mean()
+            print(f"AR@{max_detections}: \t {ar_cat:.1%}")
+
+
+if __name__ == '__main__':
+    # parser = argparse.ArgumentParser(description='')
+    # args = parser.parse_args()
+
+    # INPUT VARIABLES
+    tile_directory = Path('/Users/jilliana/Documents/rcg_projects/RuthJoy/Cormorants/cormorants-nesting-scripts-bkp/object_detection_scripts/1_preprocessing_annotation_pipeline/tile_tifs/output/2020_SNB/MANUAL_COUNTS/')
+    annotation_dir = '/Users/jilliana/Documents/rcg_projects/RuthJoy/Cormorants/cormorants-nesting-scripts/object_detection_scripts/4_comparing_manual_counts/manual_counts/output/2020/VALIDATION'
+    detections_dir = '/Users/jilliana/Documents/rcg_projects/RuthJoy/Cormorants/cormorants-nesting-scripts/object_detection_scripts/3_prediction_pipeline_postprocessing/post_process_detections/output/2020/VALIDATION/snb5_cn_hg_v9'
+
+    # OUTPUT VARIABLES
+    annotation_out_file = "~/Downloads/annotations.json"
+    detection_result_file = "~/Downloads/results.json"
+
+    # Annotations
+    create_annotation_coco_json(tile_directory,
+                                annotation_dir,
+                                annotation_out_file)
+
+    # Detections
+    create_detection_coco_json(tile_directory,
+                               detections_dir,
+                               detection_result_file)
+
+    coco_eval = evaluate_from_json_file(annotation_out_file,
+                                        detection_result_file,
+                                        max_detections=[20, 200, 400],
+                                        iou_thresholds=[0.5],
+                                        out_file="/Users/jilliana/Downloads/coco_eval_iou0.5.csv")
+
+    calculate_precisions(coco_eval, iou_threshold=0.1)
+    calculate_recalls(coco_eval, 400)
+    calculate_recalls(coco_eval, 400, 0.1)
+    calculate_recalls(coco_eval, 200)
+    calculate_recalls(coco_eval, 200, 0.1)
--- object_detection_scripts/4_comparing_manual_counts/manual_counts/src/manual_counts.py
+++ object_detection_scripts/4_comparing_manual_counts/manual_counts/src/manual_counts.py
@@ -7,6 +7,13 @@ python3 manual_counts.py \
 --label_tree_file /Users/jilliana/Documents/rcg_projects/RuthJoy/Cormorants/cormorants-nesting-scripts/object_detection_scripts/combine_anno_labels/input/snb6/cormorants_2022_label_tree.csv \
 --desired_labels Cormorant Nest
 
+python3 manual_counts.py \
+--anno_folder ../../../../../cormorants-nesting-scripts-bkp/object_detection_scripts/4_comparing_manual_counts/manual_counts/input/SNB_2020/VALIDATION/manual_annotations \
+--removal_record ../../../../../cormorants-nesting-scripts-bkp/object_detection_scripts/4_comparing_manual_counts/manual_counts/input/SNB_2020/VALIDATION/RemovalRecord.xlsx \
+--output_file ../output/SNB_2020/VALIDATION/manual_counts_verified.csv \
+--label_tree_file /Users/jilliana/Documents/rcg_projects/RuthJoy/Cormorants/cormorants-nesting-scripts/object_detection_scripts/combine_anno_labels/input/snb6/cormorants_2022_label_tree.csv \
+--desired_labels Cormrant Nest
+
 """
 import argparse
 import ast
@@ -14,7 +21,37 @@ from pathlib import Path
 from datetime import datetime
 import re
 import pandas as pd
+import numpy as np
 import networkx as nx
+from shapely.ops import unary_union
+from shapely.geometry import box
+
+
+def get_bbox_coords(raw_anno):
+    if pd.isna(raw_anno):
+        xmin = -1
+        ymin = -1
+        xmax = -1
+        ymax = -1
+    else:
+        anno_dict = ast.literal_eval(raw_anno)
+        xmin = (anno_dict['x']-anno_dict['w']/2)
+        ymin = (anno_dict['y']-anno_dict['h']/2)
+        xmax = (anno_dict['x']+anno_dict['w']/2)
+        ymax = (anno_dict['y']+anno_dict['h']/2)
+
+    return xmin, ymin, xmax, ymax
+
+
+def create_detection_geom(tile_name, detection_box):
+    y_tile, x_tile = Path(tile_name).name.split('.')[:2]
+    y_tile = int(y_tile)
+    x_tile = int(x_tile)
+
+    x1, y1, x2, y2 = detection_box  # format: [y1, x1, y2, x2]
+    b = box(minx=x_tile+x1, miny=y_tile+y1,
+            maxx=x_tile+x2, maxy=y_tile+y2)
+    return b
 
 
 def read_label_tree(label_tree_file):
@@ -46,6 +83,55 @@ def label2agglabel(label_tree, desired_labels):
     return l2l_map
 
 
+def lost_to_geom(img_path, anno):
+    box = create_detection_geom(tile_name=img_path,
+                                detection_box=get_bbox_coords(anno))
+    return box
+
+
+def merge_duplicate_annotations(df, duplicates):
+    non_annotations = df[df['anno.idx'].isna()]
+
+    df = df.dropna(subset=['anno.idx']).set_index('anno.idx')
+
+    canonical_detection_map = {}
+    polygons_to_merge = {idx: [(idx, lost_to_geom(img, anno))] for idx, img, anno in
+                         zip(df.index,
+                             df['img.img_path'],
+                             df['anno.data']) if idx in list(duplicates['index_x'])}
+
+    for i_x, i_y in zip(duplicates['index_x'], duplicates['index_y']):
+        box_y = lost_to_geom(df['img.img_path'].loc[i_y],
+                             df['anno.data'].loc[i_y])
+
+        if i_x in canonical_detection_map:
+            canonical_detection = canonical_detection_map[i_x]
+        else:
+            canonical_detection = i_x
+
+        polygons_to_merge[canonical_detection].append((i_y, box_y))
+        canonical_detection_map[i_y] = canonical_detection
+
+    merged_detections = {}
+    for canonical_index in polygons_to_merge:
+        polygons = [p for i, p in polygons_to_merge[canonical_index]]
+        if len(polygons):
+            merged_detection = str(list(unary_union(polygons).bounds))
+            merged_detections[canonical_index] = merged_detection
+
+    for i, d in df["anno.data"].items():
+        if i in merged_detections:
+            converted_box = merged_detections[i]
+        elif isinstance(d, str):
+            tile_path = df['img.img_path'].loc[i]
+            converted_box = str(list(lost_to_geom(tile_path, d).bounds))
+        elif np.isnan(d):
+            converted_box = d
+        df.loc[i, 'detection_boxes'] = converted_box
+
+    return pd.concat([non_annotations, df.reset_index()])
+
+
 def main(anno_folder, removal_record, out_file, label_tree_file=None,
          desired_labels=['Cormorant', 'Nest']):
 
@@ -64,6 +150,24 @@ def main(anno_folder, removal_record, out_file, label_tree_file=None,
 
         # Filter removal record to only include annotations from the relevant date
         date_annos_to_remove = annos_to_remove[annos_to_remove['Date'] == file_date]
+
+        # Remove Incorrect Annotations
+        incorrect_annos = date_annos_to_remove[date_annos_to_remove['Reason'] == 'not a bird or nest']['Annotation ID']
+        temp_annos = annotation_df[~annotation_df['anno.idx'].isin(incorrect_annos)]
+
+        # Remove Out-Of-Bounds Annotations
+        out_of_bounds_annos = date_annos_to_remove[date_annos_to_remove['Reason'] == 'outside masking area']['Annotation ID']
+        temp_annos = temp_annos[~temp_annos['anno.idx'].isin(out_of_bounds_annos)]
+
+        # Merge Duplicate Annotations
+        duplicate_annos = date_annos_to_remove[date_annos_to_remove['Reason'] == 'duplicate over tiles'][['Canonical Annotation', 'Annotation ID']]
+        duplicate_annos.columns = ['index_x', 'index_y']
+        temp_annos = merge_duplicate_annotations(temp_annos, duplicate_annos)
+        duplicate_indices = duplicate_annos['index_y']
+        temp_annos = temp_annos[~temp_annos['anno.idx'].isin(list(duplicate_indices))]
+
+        verified_annotations = temp_annos
+        verified_annotations.to_csv(out_file.parent.joinpath(f"verified_{file.name}"))
 
         # Remove Annotations
         verified_annotations = annotation_df[~annotation_df['anno.idx'].isin(date_annos_to_remove['Annotation ID'])]
@@ -99,3 +203,19 @@ if __name__ == '__main__':
 
     main(Path(args.anno_folder), Path(args.removal_record), Path(args.output_file),
          label_tree_file=args.label_tree_file, desired_labels=args.desired_labels)
+
+    # main(
+    #     anno_folder=Path("/Users/jilliana/Documents/rcg_projects/RuthJoy/Cormorants/cormorants-nesting-scripts/object_detection_scripts/4_comparing_manual_counts/manual_counts/input/2020/VALIDATION/manual_annotations"),
+    #     removal_record=Path("/Users/jilliana/Documents/rcg_projects/RuthJoy/Cormorants/cormorants-nesting-scripts/object_detection_scripts/4_comparing_manual_counts/manual_counts/input/2020/VALIDATION/RemovalRecord_CanonicalAnnotation.xlsx"),
+    #     out_file=Path("/Users/jilliana/Documents/rcg_projects/RuthJoy/Cormorants/cormorants-nesting-scripts/object_detection_scripts/4_comparing_manual_counts/manual_counts/output/2020/VALIDATION/manual_counts_verified.csv"),
+    #     label_tree_file="/Users/jilliana/Documents/rcg_projects/RuthJoy/Cormorants/cormorants-nesting-scripts/object_detection_scripts/4_comparing_manual_counts/manual_counts/input/2020/VALIDATION/cormorants_2022_label_tree.csv",
+    #     desired_labels=["Cormorant", "Nest"]
+    # )
+
+    main(
+        anno_folder=Path("/Users/jilliana/Documents/rcg_projects/RuthJoy/Cormorants/cormorants-nesting-scripts/object_detection_scripts/4_comparing_manual_counts/manual_counts/input/2021/TEST/manual_annotations"),
+        removal_record=Path("/Users/jilliana/Documents/rcg_projects/RuthJoy/Cormorants/cormorants-nesting-scripts/object_detection_scripts/4_comparing_manual_counts/manual_counts/input/2021/TEST/RemovalRecord_CanonicalAnnotation_2021.xlsx"),
+        out_file=Path("/Users/jilliana/Documents/rcg_projects/RuthJoy/Cormorants/cormorants-nesting-scripts/object_detection_scripts/4_comparing_manual_counts/manual_counts/output/2021/TEST/manual_counts_verified.csv"),
+        label_tree_file="/Users/jilliana/Documents/rcg_projects/RuthJoy/Cormorants/cormorants-nesting-scripts/object_detection_scripts/4_comparing_manual_counts/manual_counts/input/2021/TEST/cormorants_2022_label_tree.csv",
+        desired_labels=["Cormorant", "Nest"]
+    )
