eb1967e6df1cbc4cfefd135f59b1798bc98fda76
VWJF <3881641+VWJF@users.noreply.github.com>
2024-07-30 14:02:56 -0700

    Update label_studio.py, change comparison references with 'left', 'right' nomenclature. Implement a cmd arg for swapping left-right column names

--- object_detection_scripts/4_comparing_manual_counts/compare_detections_labelstudio/src/label_studio.py
+++ object_detection_scripts/4_comparing_manual_counts/compare_detections_labelstudio/src/label_studio.py
@@ -2,8 +2,10 @@ import argparse
 import pandas as pd
 from pathlib import Path
 import json
+import sys
+PYTHON_VERSION = tuple(map(int, sys.version.split()[0].split(".")))
 
-def main(input_file:str, compare_with, output_file):
+def main(input_file:str, right, output_file, swap:bool):
     """Reshape the input csv into a shape that can be used for a Label Studio labeling task 
     to compare individual detections from one day against the next day's panorama, the status of nest
 
@@ -14,8 +16,9 @@ def main(input_file:str, compare_with, output_file):
                         'indv_name' (path to detection png image), 
                         'detection_classes' (int), 
                         'image' (str from tiff_tiles)
-        compare_with (str): Name of panorama for day 2
-        
+        right (str): Name of panorama for day 2
+        swap (bool): Swap references left, right references. Default is False
+
         A json file containing a list of objects with the key 'data' is produced. 
         This json file can be provided to label studio to create a task. 
         The source images must have been uploaded previously to the label studio VM.
@@ -31,32 +34,45 @@ def main(input_file:str, compare_with, output_file):
     file_type = Path(sample_image).suffix # '.jpg' or '.png'
     df['detection_classes'] = df['detection_classes'].astype(int).replace({0:'nest', 1:'cormorant'})
 
-    df['pano_name'] = df['image'].map(lambda p: Path(p).parent.name).astype(str)
+    df['pano_right'] = df['image'].map(lambda p: Path(p).parent.name).astype(str)
     # df["compare_with"] = df[compare_with] if isinstance(compare_with, str) else compare_with
-    df["compare_with"] = compare_with
+    df["pano_left"] = right
 
-    image1 = URL_PREFIX + df['compare_with'] + file_type
-    image2 = URL_PREFIX + df['pano_name'] + '/detection_tiles/' + df['id'].astype(str) + file_type
+    image1 = URL_PREFIX + df['pano_left'] + file_type
+    image2 = URL_PREFIX + df['pano_right'] + '/detection_tiles/' + df['id'].astype(str) + file_type
     # images = [image1, image2, df['detection_classes']]
     # image_labels = ["image1", "image2", 'detection_classes']
-    df['image1'] = image1
-    df['image2'] = image2
+    df['image_left'] = image1
+    df['image_right'] = image2
 
     for col in ['beam_id']: # if the mandatory column doesn't exist, create an empty column
         if col not in df.columns:
             df[col] = ''
-    df = df.rename(columns={'id':'nest_id'})
+    df = df.rename(columns={'id':'detection_id'})
 
     ## for elem in zip(image1, image2):
     #    # print(dict(zip(["image1", "image2"], elem)))
     # data = [dict(zip(image_labels, elem)) for elem in zip(*images)]
     # df['data'] = data
 
+    if swap:
+        # print(df.loc[0])
+        swap_dict = {'left':'right', 'right':'left'}
+        def swap_str(name:str):
+            name = name.lower()
+            direction = 'left' if 'left' in name else 'right'
+            return name.replace(direction, swap_dict.get(direction))
+        
+        lr_cols = [ c for c in df.columns if 'left' in c.lower() or 'right' in c.lower() ]
+        lr_cols_renamed = list(map(swap_str, lr_cols))
+        df = df.rename(columns=dict(zip(lr_cols, lr_cols_renamed)))
+        # print(df.loc[0])
+
     # Shape to list of json shape
     COLS = df.columns.to_list()
     COLS = [c for c in COLS if c not in SORTED_COLUMNS]
     df = df[SORTED_COLUMNS + COLS]
-
+    
     data = df.head(2).to_dict(orient='records')
     data = [ {'data': d} for d in data ]
     print(json.dumps(data, indent=4))
@@ -73,17 +89,30 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Compare nests between two days. Parse a detections csv into a format to load into label studio for the comparison task." 
                                      "The csv must contain columns for 'index' (int), 'indv_name' (path to detection png image), 'detection_classes' (int), 'image' (from tiff_tiles)")
     parser.add_argument('--input_file', type=str, help='CSV created by `draw_detections.py`')
-    parser.add_argument('--compare_with', type=str, help='Name of panorama to compare with')
+    parser.add_argument('--right', type=str, help='Name of panorama to compare with')
     parser.add_argument('--folder_prefix', required=True, default='samples/2023_pairs/', type=str, help='The folder on the server containing the images')
     parser.add_argument('--out_folder', type=str, default="output", help='File path to output folder.')
+    if PYTHON_VERSION < (3,9):
+        parser.add_argument('--swap', 
+                            default=False, required=False, 
+                            action='store_true', 
+                            help='Swap references left, right references. Default is False, which means left_image=pano, right_image=indv.')
+        parser.add_argument('--no-swap', dest='swap', action='store_false')
+    else:
+        parser.add_argument('--swap', 
+                            type=bool, 
+                            default=False, required=False, 
+                            action=argparse.BooleanOptionalAction, 
+                            help='Swap references left, right references. Default is False, which means left_image=pano, right_image=indv.')
 
     args = parser.parse_args()
 
     input_csv_file = Path(args.input_file)
-    compare_with = args.compare_with
+    right = args.right
     folder_prefix = args.folder_prefix
     out_folder = (Path(args.out_folder))#/ input_csv_file.name).with_suffix('.json')
-    
+    swap = args.swap
+
     out_name = input_csv_file.stem + "_" + "label_studio"
     output_file = (out_folder/out_name).with_suffix(".json")
 
@@ -103,6 +132,6 @@ if __name__ == "__main__":
     URL_PREFIX = f'/data/local-files/?d={folder_prefix}'
     print(f"The URL_PREFIX={URL_PREFIX}")
 
-    SORTED_COLUMNS = ["nest_id", "beam_id", "pano_name", "compare_with", "indv_name", "image1", "image2", "image",]
+    SORTED_COLUMNS = ["detection_id", "beam_id", "pano_left", "pano_right", "indv_name", "image_left", "image_right", "image",]
     
-    main(input_csv_file, compare_with, output_file)
+    main(input_csv_file, right, output_file, swap)
